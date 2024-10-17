7da302d28a59fb5ac46d12685a9b9c4abe800b8c
kimdat7000 <datkim2k4@gmail.com>
2024-10-18 00:25:24 +0700

    update all

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\MovieController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\ShowtimeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ChairController;
 
 //MOVIES
 Route::get('movies', [MovieController::class, 'index']);
@@ -36,17 +34,3 @@ Route::get('showtimes/{id}', [ShowtimeController::class, 'show']);
 Route::post('showtimes', [ShowtimeController::class, 'store']);
 Route::put('showtimes/{id}', [ShowtimeController::class, 'update']);
 Route::delete('showtimes/{id}', [ShowtimeController::class, 'destroy']);
-
-//PRODUCT
-Route::get('products', [ProductController::class, 'index']);
-Route::post('products', [ProductController::class, 'store']);
-Route::get('products/{id}', [ProductController::class, 'show']);
-Route::put('products/{id}', [ProductController::class, 'update']);
-Route::delete('products/{id}', [ProductController::class, 'destroy']);
-
-//CHAIR
-Route::get('/chairs', [ChairController::class, 'index'])->name('chairs.index');
-Route::post('/chairs', [ChairController::class, 'store'])->name('chairs.store');
-Route::get('/chairs/{id}', [ChairController::class, 'show'])->name('chairs.show');
-Route::put('/chairs/{id}', [ChairController::class, 'update'])->name('chairs.update');
-Route::delete('/chairs/{id}', [ChairController::class, 'destroy'])->name('chairs.destroy');
