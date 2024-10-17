23ebbbc0062d36b3f7c148ac412d57838640a8d7
docaohaidang04 <127119872+docaohaidang04@users.noreply.github.com>
2024-10-18 00:31:49 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,32 +6,3 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\ShowtimeController;
 
-//MOVIES
-/* Route::get('movies', [MovieController::class, 'index']);
-Route::post('movies', [MovieController::class, 'store']);
-Route::get('movies/{id}', [MovieController::class, 'show']);
-Route::put('movies/{id}', [MovieController::class, 'update']);
-Route::delete('movies/{id}', [MovieController::class, 'destroy']); */
-
-//ACCOUNTS
-/* Route::post('register', [AccountController::class, 'register']);
-Route::post('login', [AccountController::class, 'login']);
-Route::get('accounts/{id}', [AccountController::class, 'getAccount']);
-Route::put('accounts/{id}', [AccountController::class, 'updateAccount']);
-Route::delete('accounts/{id}', [AccountController::class, 'deleteAccount']); */
-
-//ROOM
-/* Route::get('rooms', [RoomController::class, 'index']);
-Route::get('rooms/active', [RoomController::class, 'getActiveRooms']);
-Route::get('rooms/{id}', [RoomController::class, 'show']);
-Route::post('rooms', [RoomController::class, 'store']);
-Route::put('rooms/{id}', [RoomController::class, 'update']);
-Route::delete('rooms/{id}', [RoomController::class, 'destroy']); */
-
-//SHOWTIME
-/* Route::get('showtimes', [ShowtimeController::class, 'index']);
-Route::get('showtimes/{id}', [ShowtimeController::class, 'show']);
-Route::post('showtimes', [ShowtimeController::class, 'store']);
-Route::put('showtimes/{id}', [ShowtimeController::class, 'update']);
-Route::delete('showtimes/{id}', [ShowtimeController::class, 'destroy']);
- */
