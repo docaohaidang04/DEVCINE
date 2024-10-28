7247040297c79d2334630b636828b99e03a03236
kimdat7000 <datkim2k4@gmail.com>
2024-10-28 20:22:37 +0700

    seeder

---
+++ database/seeders/ChairSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Chair;
+
+class ChairSeeder extends Seeder
+{
+    public function run()
+    {
+        $rows = ['A', 'B', 'C', 'D', 'E', 'F'];
+        $idRoom = 1;
+
+        foreach ($rows as $row) {
+            for ($column = 1; $column <= 10; $column++) {
+                Chair::create([
+                    'id_room' => $idRoom,
+                    'chair_name' => $row . $column,
+                    'chair_status' => 'available',
+                    'column' => $column,
+                    'row' => $row,
+                ]);
+            }
+        }
+    }
+}
